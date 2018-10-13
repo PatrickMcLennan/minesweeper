c2f94fee41bbd4e26f42bfa7a27f3547021df24b
Patrick <patrick.a.mclennan@gmail.com>
2018-10-13 16:26:04 -0400

    workin'

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const DOM = {
+  canvas: document.querySelector('.canvas'),
+  space: document.querySelectorAll('.canvas > *'),
+  mines: document.querySelector('.minesInPlay'),
+  timer: document.querySelector('.timer'),
+};
